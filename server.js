9dea1edec5cae3ed6afbbfb46978038767f2331a
davidbinstock <binstock.d@gmail.com>
2018-07-10 21:02:13 -0500

    added mongoose and axios, created controller, routes, and models folders

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
@@ -20,6 +21,17 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/jamspot",
+  {
+    useMongoClient: true
+  },
+);
+
+// Start the API server
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
