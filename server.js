98e0e762c26e04054fa55aaf06a0b12c73311b73
Carla Garcia <cdgarcia95@gmail.com>
2018-07-10 19:53:46 -0500

    Cleaned up create react express starter files

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const app = express();
 const PORT = process.env.PORT || 3001;
-// const routes = require("./routes");
+const app = express();
 
-// Configure body parser for AJAX requests
+// Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Serve up static assets
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
-// Set up promises with mongoose
-mongoose.Promise = global.Promise;
+// Define API routes here
 
-// Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/jamspot";
-
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, (error) => {
-
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('mongoose connection is sucessful');
-    }
-
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Add routes, both API and view
-// app.use(routes);
-
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// Start the API server
-app.listen(PORT, () =>
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-);
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
