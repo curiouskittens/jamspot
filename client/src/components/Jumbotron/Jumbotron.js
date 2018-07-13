1666ccb9fb04625f70d3514231336b908940d667
Guan <heguanelvis@gmail.com>
2018-07-13 12:33:22 -0500

    added header section

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Jumbotron.css"
+
+const Jumbotron = () => (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4 jumbotron-title-style text-center">Jamspot</h1>
+            <p className="lead jumbotron-lead-style text-center">Find and connect with fellow musicians to set up a jam session, collaborate and create the next big hit!</p>
+            <div className="container text-center">
+                <Link to="/login"><button className="btn btn-primary btn-lg jumbotron-button-style">Log In</button></Link>
+                <Link to="/signup"><button className="btn btn-primary btn-lg jumbotron-button-style">Sign Up</button></Link>
+            </div>
+        </div>
+    </div>
+)
+
+export default Jumbotron;
---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../../components/Header";
+
+class Landing extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Landing;
