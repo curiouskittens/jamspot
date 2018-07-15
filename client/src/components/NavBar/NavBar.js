ec4b5963d4e589f0324e683865951a75d9c547dc
Guan <heguanelvis@gmail.com>
2018-07-15 11:50:31 -0500

    made navbar scrollable

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -3,15 +3,34 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 class NavBar extends Component {
+    state = {
+        navbarInitialClass: ["nav", "navbar-config", "navbar-height", "align-items-center", "navbar-initial-color"],
+    }
+
+    handleScroll = () => {
+        let st = window.pageYOffset;
+        if (st > 0) {
+            this.setState({ navbarInitialClass: ["nav", "navbar-config", "navbar-height", "align-items-center", "navbar-semi-transparent"] })
+        } else if (st == 0) {
+            this.setState({ navbarInitialClass: ["nav", "navbar-config", "navbar-height", "align-items-center", "navbar-initial-color"] })
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
     render() {
         return (
-            <nav className="nav navbar-config navbar-height navbar-semi-transparent align-items-center">
+            <nav className={this.state.navbarInitialClass.join(" ")}>
                 <Link to="/" className="nav-link active navbar-text-style1">Jamspot</Link>
                 <Link to="/login" className="nav-link navbar-text-style2">Log In</Link>
                 <Link to="/signup" className="nav-link navbar-text-style2">Sign Up</Link>
             </nav>
         )
     }
+
+
 }
 
 
