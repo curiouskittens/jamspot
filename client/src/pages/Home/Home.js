e63446cbec34beb7a6a1a08ed3c7b7d587fb92f4
Guan <heguanelvis@gmail.com>
2018-07-17 16:18:34 -0500

    added home page basic layout and basic responsiveness

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,6 +6,70 @@ class Home extends Component {
     render() {
         return (
             <div className="home-bg">
+                <div className="home-page-content container-fluid">
+                    <div className="d-md-flex justify-content-around">
+                        <div className="jam-section-wrapper d-block col-md-8">
+                            <div className="next-jam-section d-flex container-fluid">
+                                <div className="next-jam-info col-6">
+                                    <p>Jam Name</p>
+                                    <p>Jam Location</p>
+                                    <p>Jam Date</p>
+                                </div>
+                                <div className="next-jam-members col-6">
+                                    <p>You</p>
+                                    <p>Person 1</p>
+                                    <p>Person 2</p>
+                                    <p>Person 3</p>
+                                </div>
+                            </div>
+                            <div className="most-recent-jam-section d-flex container-fluid">
+                                <div className="most-recent-jam-info col-6">
+                                    <p>Jam Name</p>
+                                    <p>Jam Location</p>
+                                    <p>Jam Date</p>
+                                </div>
+                                <div className="most-recent-jam-members col-6">
+                                    <p>You</p>
+                                    <p>Person 1</p>
+                                    <p>Person 2</p>
+                                    <p>Person 3</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="user-simple-profile-section col-md-3">
+                            <p>Name</p>
+                            <p>Username</p>
+                            <img src="" alt="profile snapshot"/>
+                            <p>Location</p>
+                            <p>Genre</p>
+                            <p>Instruments:</p>
+                            <div className="instrument-box"></div>
+                        </div>
+                    </div>
+                    <div className="suggested-jams-section d-flex justify-content-around container-fluid">
+                        <div className="suggested-jam-box col-3">
+                            <p>Jam Name1</p>
+                            <p>Location</p>
+                            <p>Date</p>
+                            <p>Genre</p>
+                            <p>Intrument</p>
+                        </div>
+                        <div className="suggested-jam-box col-3">
+                            <p>Jam Name2</p>
+                            <p>Location</p>
+                            <p>Date</p>
+                            <p>Genre</p>
+                            <p>Intrument</p>
+                        </div>
+                        <div className="suggested-jam-box col-3">
+                            <p>Jam Name3</p>
+                            <p>Location</p>
+                            <p>Date</p>
+                            <p>Genre</p>
+                            <p>Intrument</p>
+                        </div>
+                    </div>
+                </div>
             
                 <Footer />
             </div>
