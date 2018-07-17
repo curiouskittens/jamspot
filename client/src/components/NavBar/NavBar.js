c5237524bb7319fe91b7896f9accc5fd3b8aea3d
Guan <heguanelvis@gmail.com>
2018-07-17 14:17:14 -0500

    fixed navbar collapse and responsiveness

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,6 +5,8 @@ import "./NavBar.css";
 class NavBar extends Component {
     state = {
         navbarInitialClass: ["nav", "navbar-config", "navbar-height", "align-items-center", "navbar-initial-color", "navbar", "navbar-expand-lg"],
+        navtoggleInitialClass: "navbar-toggler-text",
+        collapsed: false
     }
 
     componentDidMount() {
@@ -15,11 +17,13 @@ class NavBar extends Component {
         let st = window.pageYOffset;
         if (st > 0) {
             this.setState({
-                navbarInitialClass: ["nav", "navbar-config", "navbar-height", "align-items-center", "navbar-semi-transparent", "navbar", "navbar-expand-lg"]
+                navbarInitialClass: ["nav", "navbar-config", "navbar-height", "navbar-semi-transparent", "navbar", "navbar-expand-lg"],
+                navtoggleInitialClass: "navbar-toggler-text-clicked",
             })
         } else if (st === 0) {
             this.setState({
-                navbarInitialClass: ["nav", "navbar-config", "navbar-height", "align-items-center", "navbar-initial-color", "navbar", "navbar-expand-lg"]
+                navbarInitialClass: ["nav", "navbar-config", "navbar-height", "navbar-initial-color", "navbar", "navbar-expand-lg"],
+                navtoggleInitialClass: "navbar-toggler-text",
             })
         }
     }
@@ -36,28 +40,28 @@ class NavBar extends Component {
                 <Link to="/" className="nav-link active navbar-text-style1">Jamspot</Link>
                 { this.props.loggedIn ? (
                     <React.Fragment>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span>Account</span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <Link to="/profile" className="nav-link navbar-text-style2">Profile</Link>
-                                </li>
-                                <li className="nav-item">
-                                <Link to="/myjams" className="nav-link navbar-text-style2">My Jams</Link>
-                                </li>
-                                <li className="nav-item">
-                                <Link to="/createjam" className="nav-link navbar-text-style2">Create A Jam</Link>
-                                </li>
-                                <li className="nav-item">
-                                <Link to="/findjam" className="nav-link navbar-text-style2">Find A Jam</Link>
-                                </li>
-                                <li className="nav-item">
-                                <Link onClick={this.handleLogout} to="/" className="nav-link navbar-text-style2">Log Out</Link>
-                                </li>
-                            </ul>
-                        </div>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className={this.state.navtoggleInitialClass}>⇓⇓⇓</span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav to-the-right text-center">
+                                    <li className="nav-item active">
+                                        <Link to="/profile" className="nav-link navbar-text-style2">Profile</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                    <Link to="/myjams" className="nav-link navbar-text-style2">My Jams</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                    <Link to="/createjam" className="nav-link navbar-text-style2">Create A Jam</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                    <Link to="/findjam" className="nav-link navbar-text-style2">Find A Jam</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                    <Link onClick={this.handleLogout} to="/" className="nav-link navbar-text-style2">Log Out</Link>
+                                    </li>
+                                </ul>
+                            </div>
                     </React.Fragment>
                 ) : (
                     <React.Fragment>
