045bd056f54b05275d90d42ccf6c9ef68f868c77
davidbinstock <binstock.d@gmail.com>
2018-07-17 21:46:24 -0500

    moved instrument input to own component

--- client/src/pages/CreateJam/CreateJam.js
+++ client/src/pages/CreateJam/CreateJam.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import api from "../../utils/api";
 import "./CreateJam.css";
 import Footer from "../../components/Footer";
+import InstrumentInput from "../../components/InstrumentInput"
 
 class CreateJam extends Component {
     state = {
@@ -131,30 +132,13 @@ class CreateJam extends Component {
                         <br/><br/>
                         <h4>instruments</h4>
                         {this.state.instruments.map((instrument, idx) => (
-                            <div className="instrument" key={`${idx}`}>
-                                <select
-                                    placeholder={`instrument #${idx + 1} name`}
-                                    value={instrument.name}
-                                    onChange={this.handleInstrumentNameChange(idx)}
-                                >
-                                    <option defaultValue disabled value=""> -- select an instrument -- </option>
-                                    <option value="leadGuitar">Lead Guitar</option>
-                                    <option value="rhythmGuitar">Rhythm Guitar</option>
-                                    <option value="bass">Bass</option>
-                                    <option value="keys">Keys</option>
-                                    <option value="drums">Drums</option>
-                                    <option value="percussion">Percussion</option>
-                                    <option value="vocals">Vocals</option>
-                                </select>
-                                <input 
-                                    type="number"
-                                    placeholder="#"
-                                    min="1" 
-                                    max="10" 
-                                    onChange={this.handleInstrumentQuantityChange(idx)}
-                                />
-                                <button type="button" onClick={this.handleRemoveInstrument(idx)} className="small">-</button>
-                            </div>
+                            <InstrumentInput 
+                                instrument={instrument} 
+                                nameChangeHandler={this.handleInstrumentNameChange(idx)}
+                                quantityChangeHandler={this.handleInstrumentQuantityChange(idx)} 
+                                removeHandler={this.handleRemoveInstrument(idx)}
+                                key={`${idx}`}
+                            />
                         ))}
                         <button type="button" onClick={this.handleAddInstrument} className="small">Add instrument</button>
                         <br/><br/>
