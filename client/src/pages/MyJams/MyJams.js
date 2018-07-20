5d26a1240f117978ba29d49f8b3fd0f3e223f948
davidbinstock <binstock.d@gmail.com>
2018-07-19 19:11:40 -0500

    myjams page created and routing set up

---
+++ client/src/pages/MyJams/MyJams.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Jam from "../../components/Jam";
+import api from "../../utils/api";
+import Footer from "../../components/Footer";
+
+class MyJams extends Component {
+    state = {
+        jams: [""],
+        loggedIn: sessionStorage.getItem("userId") ? true : false
+    }
+    // componentDidMount() {
+    //     this.getJams();
+    // }
+
+    // getJams = () => {
+    //     api.getAllJams().then(dbJams => {
+    //         console.log(dbJams.data)
+    //         this.setState({ jams: dbJams.data });
+    //     })
+    // }
+
+    // joinJamEventHandler = (jamId) => {
+    //     console.log("join jam!\nJam ID: ", jamId)
+    //     if(this.state.loggedIn){
+    //         console.log("you are logged in\nYour user ID is: ", sessionStorage.getItem("userId"))
+    //         api.joinJamRequest({
+    //           jamId: jamId,
+    //           userId: sessionStorage.getItem("userId")
+    //         }).then(result =>{
+    //             console.log("success")
+    //         }).catch(err => console.log(err))
+    //     }else{
+    //         console.log("you are not logged in")
+    //     }
+    // }
+    
+    render() {
+        return (
+            <React.Fragment>
+                <div className="find-jam-page-bg">
+                    <div className="find-jam-page-content container-fluid">
+                        <h4>My Jams</h4>
+                        {/* {this.state.jams.map((jam,idx)=>(
+                            <Jam key={idx} jamName={jam.name} description={jam.description} jamId={jam._id} clickHandler={()=>this.joinJamEventHandler(jam._id)}/>
+                        ))} */}
+                    </div>
+                    <Footer />
+                </div>
+            </React.Fragment>
+        )
+    }
+
+
+}
+
+export default MyJams;
