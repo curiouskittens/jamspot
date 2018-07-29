74c3e78b47c620a83ae54dd801fb7e66e895acb7
Guan <heguanelvis@gmail.com>
2018-07-28 22:26:57 -0500

    fixed line-break problem on the posts, now line breaks will be shown

--- client/src/pages/Jam/Jam.js
+++ client/src/pages/Jam/Jam.js
@@ -188,7 +188,7 @@ class Jam extends Component {
                                                         {/* <div className="jampage-instrument-wrapper col-3">
                                                             {this.getInstrumentIcon(member.instruments)}
                                                         </div> */}
-                                                        <div className="col-9">{post.content}</div>
+                                                        <div className="col-9 post-content-style">{post.content}</div>
                                                     
                                                     </div>
                                                     ))
