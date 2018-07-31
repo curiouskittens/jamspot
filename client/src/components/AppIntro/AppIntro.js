a0c109f9d479e7462a3138ebe72dd214047baa61
Carla Garcia <cdgarcia95@gmail.com>
2018-07-31 16:02:14 -0500

    Playing around with icons

--- client/src/components/AppIntro/AppIntro.js
+++ client/src/components/AppIntro/AppIntro.js
@@ -3,13 +3,16 @@ import "./AppIntro.css";
 
 const AppIntro = () => (
     <div className="app-intro-wrapper container d-xl-flex justify-content-around align-items-center">
-        <div className="text-intro-section text-center col-xl-5">
+        <div className="text-intro-section text-center">
             <p className="app-intro-title-style">What is Jamspot?</p>
-            <p className="app-intro-body-text-style">There's nothing like playing live music with others who share your passion.</p>
-            <p className="app-intro-body-text-style">Whether you're a newbie or a pro, Jamspot helps you find the coolest musicians around.</p>
-            <p className="app-intro-body-text-style">Join a Jam to meet new friends, play some tunes and create the next musical masterpiece!</p>
+            <p className="app-intro-body-text-style">Whether you're a newbie or a pro, there's nothing like playing live music with others who share your passion.</p>
         </div>
-        <iframe className="col-xl-6 app-intro-iframe-style" title="Pentatonix Youtube" src="https://www.youtube.com/embed/_NWs7ntMRBA" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        {/* connect */}
+        <i class="far fa-handshake"></i>
+        {/* collaborate */}
+        <i class="far fa-comment-dots"></i>
+        {/* create */}
+        <i class="fas fa-music"></i>
     </div>
 )
 
